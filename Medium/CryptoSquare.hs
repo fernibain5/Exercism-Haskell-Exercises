a5f4635c22e16b077dd4342ea10741850e0027d7
Fernando Ballesteros <fernandoballesteros@pop-os.localdomain>
2022-09-16 18:00:22 -0700

    Medium: CryptoSquare exercise completed

--- Medium/CryptoSquare.hs
+++ Medium/CryptoSquare.hs
@@ -1,25 +1,53 @@
 module CryptoSquare  where
 
+import Data.Char
+
 type Col = Int
 type Row = Int
-type Spaces = Int
 
-data Rectangle = Rectangle Col Row Spaces
+data Rectangle = Rectangle Col Row deriving Show
 
--- encode :: String -> String
--- encode xs
---   | xs == "" = ""
---   | otherwise = seperateSentence xs (Rectangle col row remainderSqrt)
---   where  sqrtOfLength = floor . sqrt . fromIntegral $ length xs
---          remainderSqrt = mod (length xs) sqrtOfLength
---          (row, col) = case remainderSqrt of
---                         0 -> (sqrtOfLength, sqrtOfLength)
---                         otherwise -> (sqrtOfLength, sqrtOfLength + 1)
+encode :: String -> String
+encode xs
+  | xs == "" = ""
+  | otherwise = encodeAux $ separateSentence filteredSent rect
+  where lettersAndNum  = filter (\x -> isLetter x || isNumber x) xs
+        rect = getRectangle lettersAndNum
+        filteredSent = filterSentence lettersAndNum rect
 
-seperateSentence :: String -> Rectangle -> [String]
-seperateSentence xs (Rectangle col row spaces)
-  | xs == ""        = []
-  | length xs < col = ["((take col xs) :: String ) ++ replicate spaces ' '", ""]
+encodeAux :: [String] -> String
+encodeAux xs
+  | xs == []              = ""
+  | length (head xs) == 1 = takeHeads
+  | otherwise             = takeHeads ++ " " ++ encodeAux (map (drop 1) xs)
+  where takeHeads = concatMap (take 1) xs
+
+-- It necessary to pass as an argument a sentence with only letters
+getRectangle :: String -> Rectangle
+getRectangle xs
+  | xs  == "" = (Rectangle 0 0)
+  | otherwise = (Rectangle col row)
+  where sqrtCode = sqrt . fromIntegral $ length xs
+        roundSqrt = round sqrtCode
+        decimalSqrt = snd (properFraction sqrtCode)
+        (row, col) = case 0.0 == decimalSqrt || decimalSqrt > 0.5 of
+                     True  -> (roundSqrt, roundSqrt)
+                     False -> (roundSqrt, roundSqrt + 1)
+
+addSpaces :: Int -> String -> String
+addSpaces s xs = xs ++ replicate s ' '
+
+filterSentence :: String -> Rectangle -> String
+filterSentence "" _ = ""
+filterSentence xs (Rectangle col row) =   let spaces    = col * row - (length xs)
+                                              onlyLower = map toLower xs  
+                                          in  addSpaces spaces onlyLower
+
+
+separateSentence :: String -> Rectangle -> [String]
+separateSentence "" _                     = []
+separateSentence xs rect@(Rectangle col _) = take col xs : separateSentence (drop col xs) rect
+                               
+
 
                        
-  | otherwise       = map ( )
