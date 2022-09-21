b971ba330d4265ba34ce1ea7e8c48e8b09e0e6e2
Fernando Ballesteros <fernandoballesteros@pop-os.localdomain>
2022-09-21 14:44:30 -0700

    Minesweeper exercise completed

--- Medium/Minesweeper.hs
+++ Medium/Minesweeper.hs
@@ -1,17 +1,42 @@
-module Minesweeper (annotate) where
+module Minesweeper  where
 
-annotate :: [String] -> [[(a, Char)]]
-annotate board = undefined
-
--- tupledBoard :: [String] -> [[((Int, Int), Char)]]
--- tupledBoard board@(x:xs) = 
+import Data.Maybe
 
 
-mkCoordinates :: Int -> Int -> [[(Int, Int)]]
-mkCoordinates row col = take total [ [ (j, i) | i <- [1..row]] | j <- [1..col] ]
+type Col = Int
+type Row = Int
+
+data Rectangle = Rectangle Row Col deriving Show
+
+annotate :: [String] -> [String]
+annotate [ "" ] = [ "" ]
+annotate board = fmap concat $ (map . map) (\x -> if snd x /= '*'
+                                                  then case getIntBombs x of
+                                                         0 -> take 1 $ [snd x]
+                                                         otherwise -> show $ getIntBombs x
+                                                  else "*" ) boardZipped                                           
+    where getRect = Rectangle (length $ head board) (length board)
+          boardZipped = zippingBoard board (mkCoordinates getRect)
+          concatBoard = concat $ boardZipped
+          getIntBombs x = totalBombsCoord concatBoard (bombCoord getRect (fst x))
+
+
+mkCoordinates :: Rectangle-> [[(Int, Int)]]
+mkCoordinates (Rectangle row col) = take total [ [ (rw, cl) | rw <- [1..row]] | cl <- [1..col] ]
   where total = row * col
 
-zippingCoord :: [String] -> [[(Int, Int)]] -> [[((Int, Int), Char)]]
-zippingCoord [] _ = []
-zippingCoord board@(b:bs) coord@(c:cs) = ((zip c b) : []) ++ zippingCoord bs cs
-  
+zippingBoard :: [String] -> [[(Int, Int)]] -> [[((Int, Int), Char)]]
+zippingBoard [] _ = []
+zippingBoard board@(b:bs) coord@(c:cs) = ((zip c b) : []) ++ zippingBoard bs cs
+
+bombCoord :: Rectangle -> (Int, Int) -> [(Int, Int)]
+bombCoord (Rectangle row col) c@((a, b)) = 
+  [(y, x) | y <- [1..row],
+            x <- [1..col],
+            y >= a - 1 && y < a + 2 && x >= b - 1 && x < b + 2                                                                                                                   
+            ]
+
+totalBombsCoord :: [((Int, Int), Char)] -> [(Int, Int)] -> Int
+totalBombsCoord board lCoord = length . filter (== '*') . catMaybes $ map ( (flip lookup) board ) lCoord  
+
+
